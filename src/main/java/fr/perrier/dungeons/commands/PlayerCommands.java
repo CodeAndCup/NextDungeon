b7d500b1a8d55ecd9b94bab4a0567768f40a7099
“Perrier” <perrierbottle.eaupetillante@gmail.com>
2025-08-29 17:14:54 +0200

    Update Cup'Code API

--- src/main/java/fr/perrier/dungeons/commands/PlayerCommands.java
+++ src/main/java/fr/perrier/dungeons/commands/PlayerCommands.java
@@ -1,4 +1,12 @@
 package fr.perrier.dungeons.commands;
 
+import fr.perrier.cupcodeapi.commands.annotations.Command;
+import org.bukkit.entity.Player;
+
 public class PlayerCommands {
+
+    @Command(names = {"dungeon","dungeons","nextdungeon","nextdungeons"})
+    public static void onDungeonCommand(Player player) {
+
+    }
 }
